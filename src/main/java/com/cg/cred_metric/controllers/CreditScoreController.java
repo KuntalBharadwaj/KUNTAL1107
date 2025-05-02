1e480aeb1839b92ad2f1c9032c1df44ca011bc07
KuntalBharadwaj <kuntalbharadwaj18@gmail.com>
2025-05-02 14:08:33 +0530

    [Kuntal] add: adding response dto for crdit score

--- src/main/java/com/cg/cred_metric/controllers/CreditScoreController.java
+++ src/main/java/com/cg/cred_metric/controllers/CreditScoreController.java
@@ -1,14 +1,19 @@
 package com.cg.cred_metric.controllers;
 
 
+import com.cg.cred_metric.dtos.creditscore.CreditScoreDataDTO;
+import com.cg.cred_metric.dtos.creditscore.CreditScoreResponseDTO;
 import com.cg.cred_metric.services.CreditScoreService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
 
 @RestController
 @RequestMapping("/api/creditscore")
@@ -23,8 +28,12 @@ public class CreditScoreController {
     }
 
     @GetMapping("/{userId}")
-    public ResponseEntity<Integer> getCreditScore(Authentication authentication, @PathVariable Long userId) {
+    public ResponseEntity<CreditScoreResponseDTO> getCreditScore(Authentication authentication, @PathVariable Long userId) {
         int score = creditScoreService.calculateScoreForUser(userId); // This is the correct method
-        return ResponseEntity.ok(score);
+        CreditScoreDataDTO creditScoreDataDTO = new CreditScoreDataDTO(LocalDate.now(),score);
+        CreditScoreResponseDTO creditscoreresponse = new CreditScoreResponseDTO(200,
+                creditScoreDataDTO,
+                "Credit score fetched successfully");
+        return new ResponseEntity<>(creditscoreresponse, HttpStatus.OK);
     }
 }
