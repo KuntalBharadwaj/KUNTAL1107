74e515e13e29d2991aa2e1cb2be222b9693b8af4
Naman Malhotra <naman0913.be21@chitkara.edu.in>
2025-05-05 00:59:58 +0530

    [Naman Malhotra] ADD: Cron update

--- src/main/java/com/cg/cred_metric/services/CreditScoreService.java
+++ src/main/java/com/cg/cred_metric/services/CreditScoreService.java
@@ -139,7 +139,7 @@ public class CreditScoreService {
     }
 
     // Monthly Scheduler
-    //@Scheduled(cron = "0 0 0 1 * *")
+    @Scheduled(cron = "0 0 0 1 * *")
     // For testing
     // @Scheduled(cron = "0 0/1 * * * ?")
     @Transactional
